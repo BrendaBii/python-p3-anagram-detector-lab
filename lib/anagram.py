b8d4d64ac22fa2166ff50e16fb097fc35422018f
BrendaBii <bii.brenda.bcb@gmail.com>
2023-08-24 16:03:35 +0300

    Completed assignment.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        my_word = [letter for letter in self.word]
+        my_list = []
+        for item in list:
+            word_list = [l for l in item]
+            if(sorted(my_word) == sorted(word_list)):
+                my_list.append(item)
+        return my_list
